6a5207056eb65ac6f25afac7d030742f5900c196
Amr-3009 <amrosama1k@gmail.com>
2023-07-20 11:04:09 -0700

    ff

---
+++ 2-selection_sort.c
@@ -0,0 +1,32 @@
+#include "sort.h"
+
+/**
+ * selection_sort - sorts an array of nums using selection sort
+ * @size: size of array
+ * @array: array
+ */
+
+void selection_sort(int *array, size_t size)
+{
+        size_t i, index;
+        int tmp, replace, j = 0;
+
+        for (i = 0; i < size; i++)
+        {
+                tmp = i;
+                j = 0;
+                for (index = i + 1; index < size; index++)
+                {
+                        if (array[tmp] > array[index])
+                        {
+                                tmp = index;
+                                j++;
+                        }
+                }
+                replace = array[i];
+                array[i] = array[tmp];
+                array[tmp] = replace;
+                if (j != 0)
+                        print_array(array, size);
+        }
+}
