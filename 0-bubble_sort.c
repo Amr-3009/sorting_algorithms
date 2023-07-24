f5d83c382fd4dab1851b4a7a1818de80a3a18a0e
Amr-3009 <amrosama1k@gmail.com>
2023-07-24 08:00:06 -0700

    ff

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,14 +1,15 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort list with bubble
- * @array: The array to be printed
- * @size: Number of elements in @array
+ * bubble_sort - sort list with bubble method
+ * @array: input array
+ * @size: size of array
  */
+
 void bubble_sort(int *array, size_t size)
 {
 	size_t n = 0, t = size;
-	int holder;
+	int tmp;
 
 	if (size < 2)
 		return;
@@ -16,11 +17,11 @@ void bubble_sort(int *array, size_t size)
 	{
 		for (n = 0; n < size - 1; n++)
 		{
-			if (array[n] > array[n + 1])
+			if (array[n] > array[n + 1] && array[n + 1])
 			{
-				holder = array[n];
+				tmp = array[n];
 				array[n] = array[n + 1];
-				array[n + 1] = holder;
+				array[n + 1] = tmp;
 				print_array(array, size);
 			}
 		}
