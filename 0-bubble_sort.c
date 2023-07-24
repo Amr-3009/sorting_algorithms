5ceb9f87665df3327961da421b9ba7f22ddc4e26
Amr-3009 <amrosama1k@gmail.com>
2023-07-24 07:43:03 -0700

    gg

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,17 +1,13 @@
 #include "sort.h"
-
 /**
- * bubble_sort - sort array using bubble sort method
+ * bubble_sort - sort array lements from min to max value
  * @array: array
  * @size: array size
  */
-
-
 void bubble_sort(int *array, size_t size)
 {
 
 	size_t i, index, tmp = 0;
-
 
 	if (size < 2)
 		return;
@@ -20,10 +16,10 @@ void bubble_sort(int *array, size_t size)
 		{
 			if (array[index] > array[index + 1] && array[index + 1])
 			{
-				tmp = array[index];
-				array[index] = array[index + 1];
-				array[index + 1] = tmp;
-				print_array(array, size);
+			tmp = array[index];
+			array[index] = array[index + 1];
+			array[index + 1] = tmp;
+			print_array(array, size);
 			}
 		}
 }
